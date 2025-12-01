ea26bf48544887ffd1859f3a8d92f803fd261734
Alisson Gabriel <alissongabrielassuncao@gmail.com>
2025-12-01 19:42:57 -0300

    fix: corrige bug

--- PLP-source/Imperativa3/src/li3/plp/imperative3/util/TipoProcedimento.java
+++ PLP-source/Imperativa3/src/li3/plp/imperative3/util/TipoProcedimento.java
@@ -22,8 +22,22 @@ public class TipoProcedimento implements Tipo {
 	public boolean eIgual(Tipo tipo) {
 		if (tipo instanceof TipoProcedimento) {
 			TipoProcedimento tipoProc = (TipoProcedimento) tipo;
-			return tipoProc.tiposParametrosFormais
-					.equals(this.tiposParametrosFormais);
+
+			// Verificar se têm o mesmo número de parâmetros
+			if (tipoProc.tiposParametrosFormais.size() != this.tiposParametrosFormais.size()) {
+				return false;
+			}
+
+			// Comparar cada tipo de parâmetro usando eIgual() em vez de equals()
+			for (int i = 0; i < this.tiposParametrosFormais.size(); i++) {
+				Tipo tipoThis = this.tiposParametrosFormais.get(i);
+				Tipo tipoOutro = tipoProc.tiposParametrosFormais.get(i);
+				if (!tipoThis.eIgual(tipoOutro)) {
+					return false;
+				}
+			}
+
+			return true;
 		}
 
 		return tipo.eIgual(this);
